90d9a6ae74c9a4907116e9a09944be18b5f95076
muratbekov3 <akylbekmuratbekuulu@gmail.com>
2025-12-12 20:38:20 +0100

    updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Akylbeks first DevOps Project!"
+    return "Hello from Akylbeks first DevOps Project with CI!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
